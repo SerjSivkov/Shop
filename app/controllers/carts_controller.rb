554dff78fcbed07c5684a7f5e915204c33463e11
Sergey <serjsivkov@gmail.com>
2017-12-20 11:55:11 +0300

    Admin Controle Panel

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
@@ -72,6 +73,7 @@ class CartsController < ApplicationController
     def cart_params
       params.fetch(:cart, {})
     end
+    
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
       redirect_to store_url, notice: 'Invalid cart'
